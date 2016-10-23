eac34abec659adc4304c1ba9c4bf3ef66d13b374
Takaaki Hiragushi <ent@arrize.net>
2016-10-23 17:46:34 +0900

    Fix to use vertex_t

--- test/graph/sssp_dijkstra_test.cpp
+++ test/graph/sssp_dijkstra_test.cpp
@@ -6,15 +6,15 @@
 
 TEST(SSSPDijkstraTest, IntegerWeight){
 	using edge = loquat::edge<loquat::edge_param::weight<int>>;
+	std::default_random_engine engine;
 	for(const size_t n : { 2, 10, 50, 100, 500 }){
-		std::default_random_engine engine;
 		auto graph =
 			loquat::test::random_graph_generator<edge>(n, 0.05)
 				.has_self_loop(true)
 				.generate(engine);
 		loquat::test::randomize_weights(
 			graph, engine, std::uniform_int_distribution<int>(0, 100));
-		const size_t source = 1;
+		const loquat::vertex_t source = 1;
 		const auto actual = loquat::sssp_dijkstra(source, graph);
 		EXPECT_TRUE(loquat::test::validate_sssp_result(actual, source, graph));
 	}
@@ -22,15 +22,15 @@ TEST(SSSPDijkstraTest, IntegerWeight){
 
 TEST(SSSPDijkstraTest, FloatingWeight){
 	using edge = loquat::edge<loquat::edge_param::weight<double>>;
+	std::default_random_engine engine;
 	for(const size_t n : { 2, 10, 50, 100, 500 }){
-		std::default_random_engine engine;
 		auto graph =
 			loquat::test::random_graph_generator<edge>(n, 0.05)
 				.has_self_loop(true)
 				.generate(engine);
 		loquat::test::randomize_weights(
 			graph, engine, std::uniform_real_distribution<double>(0, 1.0));
-		const size_t source = 1;
+		const loquat::vertex_t source = 1;
 		const auto actual = loquat::sssp_dijkstra(source, graph);
 		EXPECT_TRUE(loquat::test::validate_sssp_result(actual, source, graph));
 	}
